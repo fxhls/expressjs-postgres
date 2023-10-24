2483016b91793ba956294aa4a5dfd8b2e7d00980
fxhls <50634782+fxhls@users.noreply.github.com>
2023-10-24 16:57:00 -0400

    ac

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import bodyParser from "body-parser";
-import express from "express";
-import pg from "pg";
+
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+const { pg } = require('pg')
+
 
 // Connect to the database using the DATABASE_URL environment
 //   variable injected by Railway
